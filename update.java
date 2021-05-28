8988092317c0196b529ce88d3ad484f2ec36a3cc
azahadsyed <azahadsyed@gmail.com>
2021-05-28 18:23:38 +0530

    New Lines added in

--- update.java
+++ update.java
@@ -2,5 +2,10 @@ new documenation added
 public static void main(String [] args)
 {
   
-  System.out.println("Hello world") ; 
+  System.out.println("Hello world") ;
+  System.out.println("Hello world") ;
+  System.out.println("Hello world") ;
+  System.out.println("Hello world") ;
+  
+  
 }
